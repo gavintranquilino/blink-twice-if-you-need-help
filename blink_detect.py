0b9d9d8ba47c9632fb7e2ee23710ede388584df2
Gavin T <gavintranquilino@gmail.com>
2024-01-13 22:53:49 -0500

    Create blink detection

--- blink_detect.py
+++ blink_detect.py
@@ -1,54 +1,84 @@
 import numpy as np
 import cv2
+import time
+
+blink_time_value = 0.4
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
 # Starting the video capture
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Failed to open camera")
     exit()
 
 ret, img = cap.read()
 
-while(ret):
+eye_open = True
+prev_eye_open = True
+is_blinking = False
+blink_count = 0
+last_blink_time = 0
+
+while ret:
     ret, img = cap.read()
 
     # Mirror image horizontally
     img = cv2.flip(img, 1)
 
-    # Coverting the recorded image to grayscale
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # Converting the recorded image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Applying filter to remove impurities
-    gray = cv2.bilateralFilter(gray,5,1,1)
+    gray = cv2.bilateralFilter(gray, 5, 1, 1)
 
-    # Detecting the face for region of image to be fed to eye classifier
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(200, 200))
-    if (len(faces) > 0):
+    # Detecting the face for the region of the image to be fed to the eye classifier
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(300, 300))
+
+    if len(faces) > 0:
         for (x, y, w, h) in faces:
-            img = cv2.rectangle(img, (x, y), (x+w, y+h),(0, 255, 0), 2)
+            img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-            # roi_face is face which is input to eye classifier
+            # roi_face is the face which is input to the eye classifier
             roi_face = gray[y:y+h, x:x+w]
-            roi_face_clr = img[y:y+h, x:x+w]
             eyes = eye_cascade.detectMultiScale(roi_face, 1.3, 5, minSize=(50, 50))
 
-            # Examining the length of eyes object for eyes
-            if (len(eyes) >= 2):
-                # Check if program is running for detection 
+            # Examining the length of the eyes object for eyes
+            if len(eyes) >= 2:
+                eye_open = True
                 cv2.putText(img, "Eyes Open", (70, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+
+                # If eyes were previously closed and blink was ongoing, end the blink
+                if is_blinking:
+                    is_blinking = False
+                    blink_count += 1
+                    
+                    # If the time since the last blink is less than 50 milliseconds, it is a double blink
+                    if time.time() - last_blink_time < blink_time_value:
+                        print("Double blink detected")
+                        last_blink_time = time.time()
+                    else:
+                        last_blink_time = time.time()
+
             else:
-                cv2.putText(img, "No eyes detected", (70, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
-            
+                eye_open = False
+                cv2.putText(img, "Blink", (70, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
+
+                # Start the blink if eyes were open in the previous frame
+                if prev_eye_open:
+                    is_blinking = True
+
+            prev_eye_open = eye_open
+
     else:
+        eye_open = False
         cv2.putText(img, "No face detected", (100, 100), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
 
     # Controlling the algorithm with keys
     cv2.imshow("Mirrored Image", img)
-    a = cv2.waitKey(1)
-    if (a == ord('q')):
+    key = cv2.waitKey(1)
+    if key == ord('q'):
         break
 
 cap.release()
